5d9ebf36f120e799c617d3e9c67bd404e5822331
sampatta h <sampattahubballi@gmail.com>
2025-08-06 00:03:59 +0530

    math

---
+++ 06_number-math.js
@@ -0,0 +1,14 @@
+const balance = 400
+// console.log(balance)
+
+const balances = new Number(100)
+// console.log(balances)
+
+// console.log(balances.toString().length)
+// console.log(balances.toFixed(2))
+const j = 45.28411
+// console.log(j.toPrecision(6))
+
+console.log(Math.min(3,4,2))
+console.log(Math.random()*10+1)
+
