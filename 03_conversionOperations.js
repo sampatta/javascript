01fdc92f3b7a03aa4059b97222cf8e29da78bd63
sampatta h <sampattahubballi@gmail.com>
2025-08-05 14:23:20 +0530

    conversion

---
+++ 03_conversionOperations.js
@@ -0,0 +1,15 @@
+let score = "33www"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+console.log(valueInNumber) //NaN actual value special type
+//nul = 0 , undefined= NaN true = 1, false = 0
+
+//"33" => 33
+//"33aa" > NaN
+//true = 1
+
+let isloggedin = 1
+let booleanIsLoggedIn = Boolean(isloggedin)
