2a4d762f6f68239998a9315abfea8a24d821dd09
sampatta h <sampattahubballi@gmail.com>
2025-08-06 01:08:26 +0530

    array

--- 02basics/01arra.js
+++ 02basics/01arra.js
@@ -1,1 +1,14 @@
-console.log("hi")
+//arrays
+
+const myArr = [0,1,2,3,4,5,true]
+const myhero = ["a", "d"]
+// const n = new Array(1,2,3,4)
+// myArr.push(22)
+// console.log(myArr)
+// console.log(n)
+// n.push(55)
+// n.pop(2)
+// console.log(n)
+h = myArr.concat(myhero)
+n = [...myArr,...myhero]
+console.log(n)
