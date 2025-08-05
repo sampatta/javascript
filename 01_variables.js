0e568189fa18ad112197c17aceaaea295ef460c8
sampatta h <sampattahubballi@gmail.com>
2025-08-05 13:20:56 +0530

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+let accountId =4;
+let name = "sam"
+var paa = "1234"
+// cont wil not change after assignment
+// let var will change can assign values after declaring variable
+accountId = 5
+//you can assign variable without assigning keyword like let var
+
+place = "dharwad"
+// []used to print multiple variable
+    /*
+    prefer not to use var bcz of issue in block scope and func scope
+    */
+let job 
+// if you not assign any value it will default add undefined 
+console.log([accountId,paa,name,place,job])
---
+++ 02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all code as new va
+//alert(3+3) //we are using node
+
+console.log(3+3); console.log("hitesh")
+// for readability dont do this
+
+let name = "sagar" //strings
+let age = 18 //number 2 to power 53
+let islogged = false //boolean
+//null vs undefined
+/*
+null stand alone value "empty value"
+undefined where value is not assigned
+*/
+//symbol = unique
+console.log(typeof null)
+// null is object
+// undefined is undefined type
