c5b8b24ce27949abec1a472ebf9d3774a65fc65b
sampatta h <sampattahubballi@gmail.com>
2025-08-05 15:12:46 +0530

    datatype summary

---
+++ datatype-summary.js
@@ -0,0 +1,24 @@
+// premitive and non premitive data types
+
+//7 types strings, number, boolean, null, undefined, bigint symbol call by value
+
+//dynamically typed language 'const score =100'
+//reference type non premitive 
+//arrays objects, function
+
+const score = 100
+const scorevalue = 100.3
+
+let username 
+
+console.log(typeof username)
+
+
+const heros =["s","h","g"]
+console.log(heros)
+
+let ppp= {
+    na:"uhuih",
+    hh:77
+}
+console.log(ppp)
