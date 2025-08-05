ed1a0433554f4ed11ca8e9df95c136967b1b6e4c
sampatta h <sampattahubballi@gmail.com>
2025-08-05 14:54:44 +0530

    comparisions

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -4,7 +4,7 @@ let score = "33www"
 
 let valueInNumber = Number(score)
 // console.log(typeof valueInNumber)
-console.log(valueInNumber) //NaN actual value special type
+// console.log(valueInNumber) //NaN actual value special type
 //nul = 0 , undefined= NaN true = 1, false = 0
 
 //"33" => 33
@@ -13,3 +13,26 @@ console.log(valueInNumber) //NaN actual value special type
 
 let isloggedin = 1
 let booleanIsLoggedIn = Boolean(isloggedin)
+
+/*about operations*/
+
+let value = 3
+let negVal = -value
+// console.log(negVal)
+// basic math operations +, -,*,**,%
+console.log(2+3-3)
+
+let str1 = "ham"
+let str2 = "don"
+
+let str3 = str1+str2
+console.log(str3)
+console.log("2"+2)
+console.log(2+3+4+"2")//initial int will add 
+console.log("2"+2+5+6) //after string all will add as string
+console.log(+true)
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
---
+++ 04_comparisions.js
@@ -0,0 +1,7 @@
+
+console.log(null==0)
+console.log(null > 0)
+console.log(null >= 0)//here in converts null to 0
+
+// ===
+console.log("2" === 2)
