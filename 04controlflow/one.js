8b2a31f16b3c44b1557b2d4320239daea28ba511
sampatta h <sampattahubballi@gmail.com>
2025-08-06 14:28:07 +0530

    truth

---
+++ 04controlflow/one.js
@@ -0,0 +1,40 @@
+
+a = true
+// if (2==2){
+//     console.log("equal"
+//     );
+    
+// }
+// //< > <= >= == === != !==
+tem = 50
+
+// if (tem!=="50"){
+// console.log("good");
+// } else{
+//     console.log("bad");
+// }
+
+// const balance = 70
+
+// if (balance < 50) {
+//     console.log("greater");
+// }else if(balance < 60){
+// console.log("50-60");
+// }else if (balance< 70){
+//     console.log("60-70");
+// }else{
+//     console.log("end");
+    
+// }
+const month = 2
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    default:
+        console.log("not")
+        break;
+}
---
+++ 04controlflow/truthly.js
@@ -0,0 +1,33 @@
+// const usermail = -0
+
+// if (usermail){
+//     console.log("user email")
+// }else{
+//     console.log("dont have")
+// }
+
+//false 0, -0, Bigint 0n, "", null, undefined,NaN
+//true -44, "0","false", " ", [],{},function(){}
+
+
+const j= {name:"d"}
+
+// if ((Object.keys(j)).length===0){
+//     console.log("empty");
+// }else{
+//     console.log("one");
+    
+// }
+
+let v;
+v = undefined?? 6
+console.log(v);
+//error control
+
+
+//terniary operator
+// condition ? true : false
+
+const ice = 100
+ice >= 80 ? console.log("less 80"): console.log("k");
+
